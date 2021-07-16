97bbf73b9f13b279607999c09bb33f0568ea8397
murat <muratcaglayan430@gmail.com>
2021-07-16 13:13:42 +0300

    16.07.2021 v4

--- a/19.09.2018/kodu/Files_pc/pages/welcome.php
+++
@@ -1,6 +0,0 @@
-﻿        <div id="welcome_word">
-       		 <img src="css/images/hosgeldiniz_yazisi.jpg" width="200" height="65">
-         </div>
-         <div id=" explanation">
-         	Bu sitenin amacı toplu alışverişleri toplu yaparak kazançlı çıkmayı ilke edinmiştir. Bu amaçla yola çıkılarak bir çok kesime hitap edilmektedir. Bir çok tüketici alışverişlerini toptan yapmayı planlamaktadır. Bu plan dahilinde büyük markatelri tercih etmiştir. Bu tercihlerinin asıl nedenlerinden biri toplu  aldığı ürünleri daha ucuza alabileceğini düşünmektedir. Bu düşünceyi dahada ileri götürüyoruz ve alışverişlerinizi topluca toptan almanızı planlıyoruz. Bu ilkeyle ismimizi Topluca Topla koyduk.
-         </div>
